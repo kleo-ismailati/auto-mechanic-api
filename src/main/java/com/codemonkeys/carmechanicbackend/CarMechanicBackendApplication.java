9cc14ba6ec4d1d47ef7cd2490ef873086ec8157e
Kleo Ismailati <kismailati@gmail.com>
2021-10-13 11:00:23 +0200

    Initial setup

---
+++ src/main/java/com/codemonkeys/carmechanicbackend/CarMechanicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codemonkeys.carmechanicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMechanicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMechanicBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemonkeys/carmechanicbackend/CarMechanicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemonkeys.carmechanicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMechanicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
