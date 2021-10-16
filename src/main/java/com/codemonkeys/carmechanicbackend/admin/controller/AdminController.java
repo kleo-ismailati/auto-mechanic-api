f5a2733bf917d304903c81120d24db147c94b806
Kleo Ismailati <kismailati@gmail.com>
2021-10-16 16:17:55 +0200

    Added swagger

--- src/main/java/com/codemonkeys/carmechanicbackend/admin/controller/AdminController.java
+++ src/main/java/com/codemonkeys/carmechanicbackend/admin/controller/AdminController.java
@@ -3,19 +3,23 @@ package com.codemonkeys.carmechanicbackend.admin.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.codemonkeys.carmechanicbackend.admin.model.Admin;
 import com.codemonkeys.carmechanicbackend.admin.service.AdminService;
 
-@Controller
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
 
 	@Autowired
 	private AdminService adminService;
 	
-	@GetMapping("/admins")
+	@GetMapping
+	@ResponseBody
 	public List<Admin> getAllAdmins() {
 		return adminService.getAllAdmins();
 	}
--- src/main/java/com/codemonkeys/carmechanicbackend/client/controller/ClientController.java
+++ src/main/java/com/codemonkeys/carmechanicbackend/client/controller/ClientController.java
@@ -3,19 +3,23 @@ package com.codemonkeys.carmechanicbackend.client.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.codemonkeys.carmechanicbackend.client.model.Client;
 import com.codemonkeys.carmechanicbackend.client.service.ClientService;
 
-@Controller
+@RestController
+@RequestMapping("/client")
 public class ClientController {
 	
 	@Autowired
 	private ClientService clientService;
 	
-	@GetMapping("/clients")
+	@GetMapping
+	@ResponseBody
 	public List<Client> getAllClients() {
 		return clientService.getAllClients();
 	}
