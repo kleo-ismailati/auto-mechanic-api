edc6e7c5c605d9efae4b66f20f59e15adc87d892
Kleo Ismailati <kleoisma@hotmail.com>
2022-09-18 17:22:59 +0200

    Add initial user data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user(id, username, email, password, role) VALUES
+    (1, "monkey", "monkey@monkeyDev.com", "$2a$10$.8gxUZK4aaQAcfvC5l3pWet8a5wBPMNvNLXYNPLSEizAa6PTaAZ6q", "ADMIN")
+    ON DUPLICATE KEY UPDATE password="$2a$10$.8gxUZK4aaQAcfvC5l3pWet8a5wBPMNvNLXYNPLSEizAa6PTaAZ6q", role="ADMIN";
